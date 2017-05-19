bd2a127411c3bd1cef38f383f7b00da7cf3c708e
Lukas_Skywalker <lukas.diener@hotmail.com>
2017-05-19 10:33:21 +0200

    allow defining nulls first or last

--- lib/orderable2.rb
+++ lib/orderable2.rb
@@ -3,8 +3,13 @@ require 'orderable2/railtie' if defined?(Rails)
 module Orderable2
   extend ActiveSupport::Concern
 
-  def sort_order(order = nil)
-    calculate_order(order)
+  def sort_order(order = nil, options = {})
+    calculate_order(order).map do |column,  direction|
+      t = "#{column} #{direction}"
+      t += ' NULLS FIRST' if options.has_key?(column.to_sym) && options[column.to_sym].include?(:nulls_first)
+      t += ' NULLS LAST' if options.has_key?(column.to_sym) && options[column.to_sym].include?(:nulls_last)
+      t
+    end.join(' ')
   end
 
   private
